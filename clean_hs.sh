047e671b8876d954f67d2283a659fed9b3551dac
Lionel Zerbib <ariel.zerbib@gmail.com>
2017-02-02 16:55:42 +0200

    add clean hyperswitch

---
+++ clean_hs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rm -rf ./build
+rm -rf /usr/local/lib/python2.7/dist-packages/hypernet*
+rm -rf /usr/local/bin/hyperswitch*
+
+ovs-vsctl del-br br-vpn
+ovs-vsctl del-br br-int
+ovs-vsctl del-br br-ex
+ovs-vsctl add-br br-ex
+
+python ./setup.py clean
+python ./setup.py build
+python ./setup.py install
+
+# init conf
+rm -f /etc/init/hyperswitch*
+cp -r ./etc/agent/init/* /etc/init
+
+# etc hyperswitch conf
+rm -rf /etc/hyperswitch
+cp -r ./etc/agent/hyperswitch /etc
+
+# neutron template
+rm -rf `find /etc/neutron -name "*.tmpl"`
+cp ./etc/agent/neutron/neutron.conf.tmpl /etc/neutron
+cp ./etc/agent/neutron/plugins/ml2/ml2_conf.ini.tmpl /etc/neutron/plugins/ml2
+
+# var folder
+rm -rf /var/log/hyperswitch
+mkdir /var/log/hyperswitch
+
+rm -f /etc/hyperswitch/hyperswitch.conf
+rm -f /etc/neutron/neutron.conf
+rm -f /etc/neutron/plugins/ml2/openvswitch_agent.ini
